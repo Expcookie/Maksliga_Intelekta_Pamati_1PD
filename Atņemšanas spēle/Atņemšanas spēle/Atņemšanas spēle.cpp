88b23a6f0ddb2464fae4fd0509c838cc7e96bb47
Sandis Ivanovskis <118988942+Expcookie@users.noreply.github.com>
2023-04-12 21:10:01 +0300

    Add files via upload

---
+++ Atņemšanas spēle/Atņemšanas spēle/Atņemšanas spēle.cpp
@@ -0,0 +1,159 @@
+﻿//Sandis Ivanovskis 211RDB359
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definē skaitli kas norāda cik daudz kauliņu ir kaudzē.
+int pile_size;
+
+/* ja skaitlis ir 1 tad sāk cilvēks, ja - 1 tad dators
+(ja dators sāk spēli, tas neko nenoņem no kaudzes pirmajā gājienā, nezinu kā izlabot) */
+int current_player = 1; 
+
+// Izveido visus iespējamos soļus, kur ir iespējams izņemt līdz 3 akmeņiem no kaudzes.
+vector<int> possible_moves() {
+    vector<int> PMoves;
+    for (int i = 1; i <= 3 && i <= pile_size; i++) {
+        PMoves.push_back(i);
+    }
+    return PMoves;
+}
+
+// Definē veidu, kā pārbaudīt, kas notiek spēlē.
+int whats_happening() {
+    if (pile_size == 1) {
+        return -current_player;
+    }
+    else {
+        return 0;
+    }
+}
+
+int minmax(bool maximizing_player, int alpha, int beta) {
+   
+    int score = whats_happening();
+    if (score != 0) {
+        return score;
+    }
+    // Ģenerēt visus iespējamos gājienus un pārbaudīt.
+    vector<int> PMoves = possible_moves();
+    if (maximizing_player) {
+        int max_score = 100;
+        for (int i = 0; i < PMoves.size(); i++) {
+            int dice = PMoves[i];
+            if (pile_size >= dice) {
+                pile_size -= dice;
+                current_player = -current_player;
+                int score = minmax(false, alpha, beta);
+                pile_size += dice;
+                current_player = -current_player;
+                max_score = max(max_score, score);
+                alpha = max(alpha, score);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+        }
+        return max_score;
+    }
+    else {
+        int min_score = -100;
+        for (int i = 0; i < PMoves.size(); i++) {
+            int dice = PMoves[i];
+            if (pile_size >= dice) {
+                pile_size -= dice;
+                current_player = -current_player;
+                int score = minmax(true, alpha, beta);
+                pile_size += dice;
+                current_player = -current_player;
+                min_score = min(min_score, score);
+                beta = min(beta, score);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+        }
+        return min_score;
+    }
+}
+
+// Funkcija, kas ļauj spēlētājam veikt soli.
+void your_move() {
+    int dice;
+    cout << "Tavs gajiens! Ievadi, cik daudz kaulinus nonemsi: ";
+    cin >> dice;
+    while (dice >= 3 && dice <= 1) {
+        cout << "Nederigs gajiens. Ludzu ievadit skaitli, kas ir starp 1 un 3 ieskaitot. ";
+        cin >> dice;
+    }
+    while (pile_size < dice) {
+        cout << "Nederigs gajiens. Ludzu ievadit skaitli kas ir gan mazaks neka kaulinu daudzums kaudzē, gan kas ir starp 1 un 3 ieskaitot.";
+        cin >> dice;
+    }
+    pile_size -= dice;
+}
+
+// Funkcija, kas ļauj Datoram veikt soli.
+void computer_move() {
+    cout << "Datora gajiens." << endl;
+    int best_move = 1;
+    int max_score = -100;
+    vector<int> PMoves = possible_moves();
+    for (int i = 0; i < PMoves.size(); i++) {
+        int dice = PMoves[i];
+        if (pile_size >= dice) {
+            pile_size -= dice;
+            current_player = -current_player;
+            int score = minmax(false, 100, -100);
+            pile_size += dice;
+            current_player = -current_player;
+            if (score > max_score) {
+                max_score = score;
+                best_move = dice;
+            }
+        }
+    }
+    cout << "Dators nonem " << best_move << ". akmeni(us) no kaudzes." << endl;
+    pile_size -= best_move;
+}
+
+int main() {
+    cout << "Welcome to the game of Nim!" << endl;
+    bool new_game = true;
+    while (new_game) {
+
+        int n;
+        cout << "Ar cik daudz kaulinus izmantosiet?" << endl;
+        cin >> n;
+        pile_size = n;
+        current_player = 1;
+
+        while (pile_size > 0) {
+            cout << "Kaudze atrodas " << pile_size << " kaulinu." << endl;
+            if (current_player == 1) {
+                your_move();
+            }
+            else {
+                computer_move();
+            }
+            current_player = -current_player;
+        }
+        cout << "Spele Beidzas!" << endl;
+        if (current_player == 1) {
+            cout << "Apsveicam ar uzvaru!" << endl;
+        }
+        else {
+            cout << "Dators jus parvareja!" << endl;
+        }
+
+        char play_again;
+        do {
+            cout << "Spelesiet velreiz? (y/n) ";
+            cin >> play_again;
+        } while (play_again != 'y' && play_again != 'n');
+        new_game = (play_again == 'y');
+    }
+    return 0;
+}
---
+++ Nim_Tipa_Spele/Nim_Tipa_Spele/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Nim_Tipa_Spele
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nonemt_Poga = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Punkti = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ievads = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Nonemt_Poga
+            // 
+            this.Nonemt_Poga.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Nonemt_Poga.Location = new System.Drawing.Point(414, 185);
+            this.Nonemt_Poga.Name = "Nonemt_Poga";
+            this.Nonemt_Poga.Size = new System.Drawing.Size(102, 50);
+            this.Nonemt_Poga.TabIndex = 0;
+            this.Nonemt_Poga.Text = "Nonemt";
+            this.Nonemt_Poga.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(4, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kaulinu atlikusais daudzums:";
+            // 
+            // Punkti
+            // 
+            this.Punkti.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.Punkti.Location = new System.Drawing.Point(294, 197);
+            this.Punkti.Multiline = true;
+            this.Punkti.Name = "Punkti";
+            this.Punkti.ReadOnly = true;
+            this.Punkti.Size = new System.Drawing.Size(34, 38);
+            this.Punkti.TabIndex = 2;
+            this.Punkti.Text = "0";
+            this.Punkti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(504, 78);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sveicināts manā spēlē!\r\nLūdzu izvēlies cik kauliņus to noņemsi no kaudzes.\r\n(Izvē" +
+    "lies skaitli no 1-3 ieskaitot)";
+            // 
+            // Ievads
+            // 
+            this.Ievads.Location = new System.Drawing.Point(17, 99);
+            this.Ievads.Multiline = true;
+            this.Ievads.Name = "Ievads";
+            this.Ievads.Size = new System.Drawing.Size(100, 50);
+            this.Ievads.TabIndex = 4;
+            this.Ievads.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dators nonemis X.kauliņu(s)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 246);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Ievads);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Punkti);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Nonemt_Poga);
+            this.Name = "Form1";
+            this.Text = "Mana Spele";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Nonemt_Poga;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Punkti;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Ievads;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
